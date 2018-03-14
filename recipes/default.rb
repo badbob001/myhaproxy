b783c73cb529ac78646d2b507d66f1609806ef2c
badbob001 <badbob@gmail.com>
2018-03-14 10:44:56 -0400

    version 0.2.0, added load balancing member

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [
+{
+'hostname' => 'ec2-52-91-169-80.compute-1.amazonaws.com',
+'ipaddress' => '52.91.169.80',
+'port' => 80,
+'ssl_port' => 80
+},{
+'hostname' => 'ec2-54-165-24-114.compute-1.amazonaws.com',
+'ipaddress' => '54.165.24.114',
+'port' => 80,
+'ssl_port' => 80
+}
+]
+
+include_recipe 'haproxy::manual'
